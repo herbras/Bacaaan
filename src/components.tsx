a191fd8727041effcc1284e454884c432623d648
Ibrahim Nurul Huda <42539382+herbras@users.noreply.github.com>
2024-04-24 23:20:48 +0700

    yang pentng aoa? yak Jalan

--- src/components.tsx
+++ src/components.tsx
@@ -1,5 +1,5 @@
-import { html } from 'hono/html';
-import { jsxRenderer } from 'hono/jsx-renderer';
+import { html } from "hono/html";
+import { jsxRenderer } from "hono/jsx-renderer";
 
 export const renderer = jsxRenderer(({ children }) => {
   return html`
@@ -10,47 +10,121 @@ export const renderer = jsxRenderer(({ children }) => {
         <script src="https://unpkg.com/htmx.org@1.9.3"></script>
         <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
         <script src="https://cdn.tailwindcss.com"></script>
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image" content="https://i.ibb.co/6tR6wfK/og.png" />
+        <meta
+          name="twitter:image:src"
+          content="https://i.ibb.co/6tR6wfK/og.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image:width" content="1200" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta property="og:image:height" content="600" />
+        <meta name="twitter:image:height" content="600" />
+        <link
+          rel="icon"
+          href="https://arbain.sarbeh.com/favicon.ico"
+          sizes="any"
+        />
         <title>Referensi Kutub Mahzab Syafii dan Hanbali</title>
       </head>
       <body>
-        <section class="bg-gray-50">
-          <div class="mx-auto max-w-screen-xl px-4 py-32 lg:h-screen lg:items-center">
-              ${children}
+        <section class="bg-gray-50 ">
+          <div
+            class="mx-auto max-w-screen-xl px-4 py-32 lg:items-center"
+          >
+            ${children}
           </div>
         </section>
+        <!-- Tombol "Kembali ke Atas" -->
+        <button id="scrollToTopBtn" class="fixed bottom-4 right-4 bg-gray-800 text-white px-4 py-2 rounded-md hidden z-10">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
+        </svg>
+      </button>
+      
+      <div role="button" aria-label="Button Api Documentation" class="fixed bottom-4 left-4 z-20">
+        <a href="/reference" target="_blank" class="bg-gray-800 hover:bg-gray-900 text-white text-lg font-bold p-3 rounded-full decoration-none shadow-lg flex items-center">
+          API Doc
+        </a>
+      </div>
+<script>
+// Fungsi untuk menggulir ke bagian atas halaman
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Fungsi untuk menampilkan tombol "Kembali ke Atas" saat pengguna menggulir ke bawah
+window.onscroll = function() {
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  if (window.pageYOffset > 20) {
+    scrollToTopBtn.classList.remove('hidden');
+  } else {
+    scrollToTopBtn.classList.add('hidden');
+  }
+};
+
+// Menambahkan event listener untuk tombol "Kembali ke Atas"
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+scrollToTopBtn.addEventListener('click', scrollToTop);</script>
       </body>
     </html>
   `;
 });
 
 export const BookCard = ({ book }) => (
-  <div class="bg-white rounded-lg shadow-md p-4">
+  <div class="bg-white grid gap-6 rounded-lg shadow-md p-4">
     <div class="mb-4">
       <h3 class="text-xl font-semibold">{book.name}</h3>
     </div>
-    <div class="flex justify-end">
+
+    <div class="flex justify-between">
+      <div
+        class={`px-2 py-1 rounded-full font-bold text-white ${
+          book.category_id === 2 ? "bg-green-500" : "bg-blue-300"
+        }`}
+      >
+        {book.category_id === 2 ? "Hanbali" : "Syafii"}
+      </div>
       <a
         href={book.download_url}
         class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
         target="_blank"
-        hx-boost="true"
+        hxBoost="true"
       >
         Unduh
       </a>
     </div>
   </div>
 );
-export const SearchForm = ({ onSearch }) => (
-  <form hx-post="/search" hx-target="#search-results" class="flex flex-col md:flex-row gap-3 items-center justify-center mt-8">
+export const SearchForm = ({ onSearch, categories }) => (
+  <form
+    hx-post="/search"
+    hx-target="#search-results"
+    hx-swap="outerHTML"
+    class="flex flex-col md:flex-row gap-3 items-center justify-center mt-8"
+  >
     <input
       type="text"
       name="query"
       placeholder="Cari buku..."
       class="input input-bordered w-full py-3 input-primary"
     />
-    
-    <button type="submit"         class="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
->
+    <select
+      name="categoryId"
+      class="input input-bordered w-full py-3 input-primary"
+    >
+      <option value="">Semua Kategori</option>
+      {categories.map((category) => (
+        <option key={category.id} value={category.id}>
+          {category.name}
+        </option>
+      ))}
+    </select>
+    <button
+      type="submit"
+      class="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+    >
       Cari
     </button>
   </form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { BookCard, SearchForm, renderer } from './components';
 
 import { Hono } from 'hono';
-import { z } from 'zod';
-import { zValidator } from '@hono/zod-validator';
+import { apiReference } from '@scalar/hono-api-reference';
+import { z } from '@hono/zod-openapi'
 
 type Bindings = {
   DB: D1Database;
@@ -16,55 +16,95 @@ type Referensi = {
   download_url: string;
 };
 
+
+const ReferensiSchema = z.object({
+  id: z.number().int().openapi({ example: 1 }),
+  name: z.string().openapi({ example: 'Referensi 1' }),
+  'folder_id': z.string().openapi({ example: '1RCO_AjI3i-nTuXpbTbTGRbRwt2XFaDOl' }),
+  'file_id': z.string().openapi({ example: '1JbWVuklNFMGxHR_rGHL_rbG2PF7djF6M' }),
+  'download_url': z.string().openapi({ example: 'https://drive.google.com/uc?export=download&id=1JbWVuklNFMGxHR_rGHL_rbG2PF7djF6M' }),
+  'category_id': z.number().int().optional().openapi({ example: 2 }),
+})
+
+const PaginationSchema = z.object({
+  query: z.string().optional().openapi({ description: 'Kata kunci pencarian' }),
+  page: z.number().int().default(1).openapi({ description: 'Nomor halaman' }),
+  limit: z.number().int().default(10).openapi({ description: 'Jumlah hasil per halaman' }),
+})
+
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('*', renderer);
 
 app.get('/', async (c) => {
-  const { results } = await c.env.DB.prepare(`
-    SELECT *
-    FROM referensi
-    ORDER BY RANDOM()
-    LIMIT 9;
+  const { results: books } = await c.env.DB.prepare(`
+    SELECT * FROM referensi ORDER BY RANDOM() LIMIT 9;
   `).all<Referensi>();
 
-  const books = results;
-
+  const { results: categories } = await c.env.DB.prepare(`
+  SELECT * FROM category ORDER BY id;
+`).all<{ id: number; name: string }>();
   return c.render(
     <>
-    
-    <h1 class="text-3xl font-extrabold sm:text-5xl text-center">
-      Cari Referensi
-      <strong class="font-extrabold text-red-700 sm:block"> Kutub Mahzab Syafii dan Hanbali</strong>
-    </h1>
-    <SearchForm onSearch={(query) => c.redirect(`/search?query=${encodeURIComponent(query)}`)} />
-  
-    <div id="search-results" class="grid grid-cols-1 pt-5 md:grid-cols-2 lg:grid-cols-3 gap-4">
-  {books.map((book) => (
-    <BookCard key={book.id} book={book} />
-  ))}
-</div>
-  </>
+      <h1 class="text-3xl font-extrabold sm:text-5xl text-center">
+        Cari Referensi{' '}Kutub
+        <strong class="font-extrabold sm:block">
+           Mahzab <span class="text-blue-600">Syafii</span> dan <span class="text-green-600">Hanbali</span>
+        </strong>
+      </h1>
+      <SearchForm
+  onSearch={(query, categoryId) =>
+    c.redirect(
+      `/search?query=${encodeURIComponent(query)}${
+        categoryId ? `&categoryId=${categoryId}` : ''
+      }`
+    )
+  }
+  categories={categories}
+/>
+      <div id="search-results" class="grid grid-cols-1 pt-5 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {books.map((book) => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </div>
+    </>
   );
 });
+
+
+
 
 app.post('/search', async (c) => {
   const body = await c.req.parseBody();
   const query = body.query;
+  const categoryId = body.categoryId || null;
 
   if (query) {
-    const stmt = c.env.DB.prepare(`
-      SELECT referensi.*
-      FROM referensi
-      JOIN referensi_fts ON referensi.name = referensi_fts.name
-      WHERE referensi_fts MATCH ?
-    `).bind(query);
+    let stmt;
+    if (categoryId) {
+      stmt = c.env.DB.prepare(`
+        SELECT id, * FROM referensi
+        WHERE category_id = ? AND name IN (
+          SELECT name
+          FROM referensi_fts
+          WHERE referensi_fts MATCH ?
+        )
+      `).bind(categoryId, query);
+    } else {
+      stmt = c.env.DB.prepare(`
+        SELECT id, * FROM referensi
+        WHERE name IN (
+          SELECT name
+          FROM referensi_fts
+          WHERE referensi_fts MATCH ?
+        )
+      `).bind(query);
+    }
 
     const { results } = await stmt.all<Referensi>();
-    console.log(results);
 
     return c.render(
-      <div id="search-results" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div id="search-results" class="grid grid-cols-1 pt-5 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {results.map((book) => (
           <BookCard key={book.id} book={book} />
         ))}
@@ -75,5 +115,116 @@ app.post('/search', async (c) => {
     return c.json({ error: 'Istilah pencarian tidak ditemukan' });
   }
 });
+
+app.get('/referensi', async (c) => {
+  const { query, category, page = 1, limit = 10 } = c.req.query();
+  const db = c.env.DB as D1Database;
+  const offset = (page - 1) * limit;
+  const searchQuery = query ? `%${query}%` : '%';
+  const categoryFilter = category ? `AND category_id = ${category}` : '';
+
+  const countQuery = `SELECT COUNT(*) AS total FROM referensi r
+                      JOIN category c ON r.category_id = c.id
+                      WHERE r.name LIKE ?
+                      ${categoryFilter}`;
+
+  const dataQuery = `SELECT r.id, r.name, r.folder_id, r.file_id, r.download_url, c.name AS category_name, r.category_id
+                     FROM referensi r
+                     JOIN category c ON r.category_id = c.id
+                     WHERE r.name LIKE ?
+                     ${categoryFilter}
+                     ORDER BY r.id LIMIT ? OFFSET ?`;
+
+  try {
+    const countResult = await db.prepare(countQuery).bind(searchQuery).first('total');
+    const total = countResult ? countResult['total'] : 0;
+    const dataResult = await db.prepare(dataQuery).bind(searchQuery, limit, offset).all();
+    const data = dataResult.results.map((item) => {
+      const { category_id = null, ...rest } = item;
+      return ReferensiSchema.parse({ ...rest, category_id });
+    });
+
+    return c.json({ data, total, page, limit });
+  } catch (error) {
+    console.error('Error executing query:', error);
+    return c.json({ error: 'Failed to fetch data' }, 500);
+  }
+});
+
+app.get('/doc', (c) => {
+  const swaggerJson = {
+    openapi: '3.0.0',
+    info: {
+      version: '1.0.0',
+      title: 'Referensi API',
+      description: 'API ini memungkinkan Anda untuk mendapatkan daftar referensi buku berdasarkan filter pencarian dan kategori.'
+    },
+    paths: {
+      '/referensi': {
+        get: {
+          summary: 'Dapatkan daftar referensi',
+          description: 'Endpoint ini mengembalikan daftar referensi buku yang sesuai dengan filter pencarian dan kategori yang diberikan. Respons juga mencakup informasi paginasi.',
+          parameters: [
+            { name: 'query', in: 'query', schema: { type: 'string' }, description: 'Kata kunci pencarian untuk mencari referensi berdasarkan nama buku.' },
+            { name: 'category', in: 'query', schema: { type: 'integer' }, description: 'ID kategori untuk menyaring referensi berdasarkan kategori Mahzab, 1 untuk Syafii dan 2 untuk Hanbali.' },
+            { name: 'page', in: 'query', schema: { type: 'integer', default: 1 }, description: 'Nomor halaman untuk paginasi hasil.' },
+            { name: 'limit', in: 'query', schema: { type: 'integer', default: 10 }, description: 'Jumlah maksimum hasil yang akan dikembalikan per halaman.' },
+          ],
+          responses: {
+            '200': {
+              description: 'Respons sukses',
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      data: {
+                        type: 'array',
+                        items: { '$ref': '#/components/schemas/Referensi' },
+                        description: 'Daftar referensi yang sesuai dengan filter dan paginasi yang diberikan.'
+                      },
+                      total: { type: 'integer', description: 'Total jumlah referensi yang sesuai dengan filter.' },
+                      page: { type: 'integer', description: 'Nomor halaman saat ini.' },
+                      limit: { type: 'integer', description: 'Jumlah maksimum hasil per halaman.' },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    components: {
+      schemas: {
+        Referensi: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer', description: 'ID unik referensi.' },
+            name: { type: 'string', description: 'Nama referensi buku.' },
+            'folder_id': { type: 'string', description: 'ID folder tempat file referensi disimpan.' },
+            'file_id': { type: 'string', description: 'ID file referensi.' },
+            'download_url': { type: 'string', description: 'URL untuk mengunduh file referensi.' },
+            'category_id': { type: 'integer', description: 'ID kategori yang terkait dengan referensi.' },
+            'category_name': { type: 'string', description: 'Nama kategori yang terkait dengan referensi.' },
+          },
+        },
+      },
+    },
+  }
+
+  return c.json(swaggerJson)
+})
+
+app.get(
+  '/reference',
+  apiReference({
+    pageTitle: 'Referensi API Reference',
+    theme: 'purple',
+    spec: {
+      url: '/doc',
+    },
+  }),
+)
 
 export default app;
